ada676b5eb702c1f339b4246587b056436fc42f6
yyfigueroa <yyfigueroa@cps.edu>
2024-12-17 19:07:15 -0600

    Update a6_part4.py

--- part4-classification/a6_part4.py
+++ part4-classification/a6_part4.py
@@ -10,18 +10,35 @@ x = data[["Age", "EstimatedSalary", "Gender"]].values
 y = data["Purchased"].values
 
 # Step 1: Print the values for x and y
+print(x)
+print(y)
 
 # Step 2: Standardize the data using StandardScaler, 
+scaler = StandardScaler()
 
 # Step 3: Transform the data
+x_scaled = scaler.fit_transform(x)
 
 # Step 4: Split the data into training and testing data
+x_train, x_test, y_train, y_test = train_test_split(x_scaled, y, test_size=0.2, random_state=42)
+
 
 # Step 5: Fit the data
+log_reg = linear_model.LogisticRegression()
+log_reg.fit(x_train, y_train)
+
+
 
 # Step 6: Create a LogsiticRegression object and fit the data
 
 # Step 7: Print the score to see the accuracy of the model
-
+accuracy = log_reg.score(x_test, y_test)
+print("Accuracy:", accuracy)
 # Step 8: Print out the actual ytest values and predicted y values
 # based on the xtest data
+y_pred = log_reg.predict(x_test)
+
+print("Actual y_test values:")
+print(y_test)
+print("Predicted y values:")
+print(y_pred)
