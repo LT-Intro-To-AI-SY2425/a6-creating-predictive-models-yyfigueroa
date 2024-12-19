b146e00505b2e0aa4ce7aff0d44a34b0e4b15b72
yyfigueroa <yyfigueroa@cps.edu>
2024-12-18 21:53:41 -0600

    Update a5_part5.py

--- part5-unsupervised-learning/a5_part5.py
+++ part5-unsupervised-learning/a5_part5.py
@@ -8,25 +8,31 @@ data = pd.read_csv("part5-unsupervised-learning/customer_data.csv")
 x = data[["Annual Income", "Spending Score"]]
 
 #standardize the data
-
+scaler = StandardScaler()
+x_scaled = scaler.fit_transform(x)
 
 #the value of k has been defined for you
 k = 5
 
 #apply the kmeans algorithm
-
+kmeans = KMeans(n_clusters=k, random_state=42)
+kmeans.fit(x_scaled)
 
 #get the centroid and label values
-
+centroids = kmeans.cluster_centers_
+labels = kmeans.labels_
 
 #sets the size of the graph
 plt.figure(figsize=(5,4))
 
 #use a for loop to plot the data points in each cluster
 
+for i in range(k):
+            cluster_points = x[labels == i]
+plt.scatter(cluster_points["Annual Income"], cluster_points["Spending Score"], label=f"Cluster {i + 1}")
 
 #plot the centroids
-
+plt.scatter(centroids[:, 0], centroids[:, 1], marker='x', s=200, color='black', label="Centroids")
             
 #shows the graph
 plt.xlabel("Annual Income")
